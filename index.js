7805e13211ca97bbd9b41857538e767652f4efbe
Flora-Salamandre <f.strugala@yahoo.fr>
2021-04-30 19:48:18 +0200

    La feature email + quelques retouches sur les champs nom et prénom.

--- index.js
+++ index.js
@@ -4,6 +4,16 @@ function validateName() {
     inputMsg.hidden = !isEmpty(input.value);
 }
 
+function validateEmail() {
+    const input = $("#email")[0];
+    const inputMsg = $("#email-msg")[0];
+    inputMsg.hidden = !isEmpty(input.value) && isContainingAt(input.value);
+}
+
 function isEmpty(string) {
     return string.length == 0;
+}
+
+function isContainingAt(string) {
+    return string.includes("@");
 }
