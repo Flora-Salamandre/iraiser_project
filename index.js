4c652c0c0923c7d4e38c02b8dbd57a9b6362207e
Flora-Salamandre <f.strugala@yahoo.fr>
2021-05-03 12:38:23 +0200

    La feature Type-Carte. en HTML et JS

--- index.js
+++ index.js
@@ -4,6 +4,12 @@ function validateName() {
     inputMsg.hidden = !isEmpty(input.value);
 }
 
+function validateCoupleName() {
+    const input = $("#surname-couple")[0];
+    const inputMsg = $("#surname-couple-msg")[0];
+    inputMsg.hidden = !isEmpty(input.value);
+}
+
 function validateEmail() {
     const input = $("#email")[0];
     const inputMsg = $("#email-msg")[0];
@@ -11,9 +17,14 @@ function validateEmail() {
 }
 
 function isEmpty(string) {
-    return string.length == 0;
+    return string.trim().length == 0; // La méthode trim() permet de retirer les espaces blancs. La longueur 0 permet de vérifier que le champs est vide //
 }
 
 function isContainingAt(string) {
     return string.includes("@");
+}
+
+function displayCoupleFields(display) {
+    const element = $("#if-couple")[0];
+    element.hidden = !display;
 }
